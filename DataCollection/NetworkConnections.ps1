d912cda7fec19225068a06aaf51ebc7d3b6d2317
CCoffie <CalebCoffie@gmail.com>
2014-10-22 10:41:08 -0400

    Not needed anymore. being migrated to the service

--- DataCollection/NetworkConnections.ps1
+++
@@ -1,6 +0,0 @@
-################################################################################
-#                            NetworkConnections.ps1                            #
-#                 Logs any network connections to the computer                 #
-#                Created by Caleb Coffie - CalebCoffie@gmail.com               #
-################################################################################
-
