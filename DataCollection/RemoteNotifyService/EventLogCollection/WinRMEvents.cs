7373220d810e55526e3e33dd2b01470b735bb098
CCoffie <CalebCoffie@gmail.com>
2014-11-09 23:37:41 -0500

    Continued work on WinRM data collector

--- DataCollection/RemoteNotifyService/EventLogCollection/WinRMEvents.cs
+++ DataCollection/RemoteNotifyService/EventLogCollection/WinRMEvents.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -16,6 +17,8 @@ namespace RemoteNotify.EventLogCollection
     {
         private String RemoteNotifyLogsPath = "C:\\RemoteNotify\\Logs";
         private String LogName = "WinRMEvents.csv";
+        private String WindowsLogDirectory = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System)) + "Windows\\Sysnative\\winevt\\Logs";
+
         public void CollectWinRMEvents()
         {
             //Check if log path is created
@@ -41,50 +44,62 @@ namespace RemoteNotify.EventLogCollection
                     //Have it run every second
                     Thread.Sleep(1000);
 
-                    //Logs needed for WinRM
-                    EventLog WinRMEventLog = new EventLog("Microsoft-Windows-WinRM/Operational");
-                    //WinRMEventLog.Entries now contains all of the WinRM Logs
-
-                    //Check if it's an event we're interested in
-                    using (StreamWriter writer = new StreamWriter("C:\\RemoteNotify\\test.txt", true))
+                    //Check if windows log exists
+                    if (File.Exists(WindowsLogDirectory + "\\" + "Microsoft-Windows-WinRM%4Operational.evtx"))
                     {
-                        writer.WriteLine("Program Ran");
-                        foreach (EventLogEntry LogEntry in WinRMEventLog.Entries)
+                        String LastRecordedEventTime = LastLogCollectedTime();
+                        EventLogQuery WinRMEventLogQuery;
+                        
+                        if (LastRecordedEventTime == "")
                         {
-                            writer.WriteLine(LogEntry.InstanceId.ToString());
+                            WinRMEventLogQuery = new EventLogQuery(WindowsLogDirectory + "\\" + "Microsoft-Windows-WinRM%4Operational.evtx", PathType.FilePath);
+                            bool test = File.Exists(WindowsLogDirectory + "\\" + "Microsoft-Windows-WinRM%4Operational.evtx");
                         }
-                    }
-
-                    foreach(EventLogEntry LogEntry in WinRMEventLog.Entries)
-                    {
-                        //Deserialize data
-                        Hashtable EventData = null;
-                        MemoryStream EventDataStream = new MemoryStream(LogEntry.Data);
-                        IFormatter EventDataFormatter = new BinaryFormatter();
-                        EventData = (Hashtable) EventDataFormatter.Deserialize(EventDataStream);
-                        EventDataStream.Close();
-
-
-                        using (StreamWriter writer = new StreamWriter("C:\\test.txt", true))
+                        else
                         {
-                            writer.WriteLine(EventData.Keys.ToString() + "---");
+                            // Only return events since last recorded event
+                            String QueryString = "*[System[TimeCreated[@SystemTime >= \'" + LastRecordedEventTime + "\']]]";
+                            WinRMEventLogQuery = new EventLogQuery(WindowsLogDirectory + "\\" + "Microsoft-Windows-WinRM%4Operational.evtx", PathType.FilePath, QueryString);
+                            bool test = File.Exists(WindowsLogDirectory + "\\" + "Microsoft-Windows-WinRM%4Operational.evtx");
                         }
-                        //Write to log if originating by Winrm
-                        //using (StreamWriter writer = new StreamWriter(RemoteNotifyLogsPath + "\\" + LogName, true))
-                        //{
-                        //    writer.WriteLine(LogEntry.TimeGenerated.ToString(); + "," +
-                        //            DateTime.Now.ToString() + "," +
-                        //            LogEntry.TimeWritten.ToString() + "," +
-                        //            LogEntry.InstanceId.ToString() + "," +
-                        //            LogEntry.MachineName.ToString() + "," +
-                        //            LogEntry.Message.ToString() + "," +
-                        //            LogEntry.Source.ToString() + "," +
-                        //            LogEntry.UserName.ToString() + "," +
-                        //             + "," +
-                        //            Process.GetProcessById(checked((int)Connection.Connection.owningPid)).ProcessName + "," +
-                        //            Connection.ConnectionGuid.ToString()
-                        //            );
-                        //}
+
+                        EventLogReader logReader;
+                        try
+                        {
+                            // Query the log and create a stream of selected events
+                            logReader = new EventLogReader(WinRMEventLogQuery);
+                        }
+                        catch (EventLogNotFoundException)
+                        {
+                            return; //Failed to query the WinRM log!
+                        }
+
+                        for (EventRecord eventInstance = logReader.ReadEvent(); eventInstance != null; eventInstance = logReader.ReadEvent())
+                        {
+                            String eventXml = eventInstance.ToXml();
+                            Console.WriteLine(eventXml);
+                            using (StreamWriter writer = new StreamWriter(RemoteNotifyLogsPath + "\\" + LogName, true))
+                            {
+                                writer.WriteLine(eventXml);
+                            }
+                        }
+
+                            //Write to log if originating by Winrm
+                            //using (StreamWriter writer = new StreamWriter(RemoteNotifyLogsPath + "\\" + LogName, true))
+                            //{
+                            //    writer.WriteLine(LogEntry.TimeGenerated.ToString() + "," +
+                            //            DateTime.Now.ToString() + "," +
+                            //            LogEntry.TimeWritten.ToString() + "," +
+                            //            LogEntry.InstanceId.ToString() + "," +
+                            //            LogEntry.MachineName.ToString() + "," +
+                            //            LogEntry.Message.ToString() + "," +
+                            //            LogEntry.Source.ToString() + "," +
+                            //            LogEntry.UserName.ToString() + "," +
+                            //             + "," +
+                            //            Process.GetProcessById(checked((int)Connection.Connection.owningPid)).ProcessName + "," +
+                            //            Connection.ConnectionGuid.ToString()
+                            //            );
+                            //}
                     }
 
                 }
@@ -92,6 +107,39 @@ namespace RemoteNotify.EventLogCollection
                 {
                     return;
                 }
+            }
+        }
+        private String LastLogCollectedTime()
+        {
+            //Check if log path is created
+            if (!(Directory.Exists(RemoteNotifyLogsPath)))
+            {
+                // Log Directory doesn't exist yet
+                return "";
+            }
+
+            //Check if log is created
+            if (!(File.Exists(RemoteNotifyLogsPath + "\\" + LogName)))
+            {
+                //log file doesn't exist yet
+                return "";
+            }
+            String LastLine = File.ReadLines(RemoteNotifyLogsPath + "\\" + LogName).Last();
+            //Check if there's anything in the log file
+            if (LastLine == "TimeGenerated,TimeLogged,TimeCollectedByRemoteNotify,InstanceID,MachineName,Message,SourceApplication,UserResponsible,State,OwningPID,OwningProcessName,ConnectionGuid")
+            {
+                //No Events have been logged yet
+                return "";
+            }
+            else
+            {
+                int comma = LastLine.IndexOf(',');
+                string TempString = LastLine;
+                if (comma != -1)
+                {
+                    TempString = LastLine.Substring(0, comma);
+                }
+                return TempString;
             }
         }
     }
